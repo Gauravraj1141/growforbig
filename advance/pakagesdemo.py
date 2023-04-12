d454d257bfba0129c749bf5ab4c3d79402e54def
Gaurav Rajput <105104456+Gauravraj1141@users.noreply.github.com>
2023-04-12 15:19:38 +0530

    adding some string functions

--- advance/pakagesdemo.py
+++ advance/pakagesdemo.py
@@ -1,7 +1,31 @@
 import pandas
+#
+# # we can read a csv file easily and perform all the operations on it
+# data = pandas.read_csv("newdata.csv")
+# # print(data.values)
+# print(data.items)
 
-# we can read a csv file easily and perform all the operations on it
-data = pandas.read_csv("newdata.csv")
-# print(data.values)
-print(data.items)
 
+# some string functions let's deep dive into it
+
+# name = "Gaurav Rajput"
+# name = "ramv Rajput"
+#
+# print(name.casefold())  # it will convert upper case into lower case
+# print(name.isspace())
+#
+# # here we can write a logic here for write a name like title first letter is capital and other will small
+# print(name.istitle())
+
+
+def title(name):
+    if not name.istitle():
+        name = name.title()
+    return name
+
+
+data = 'ram singh'
+
+
+myname = title(data)
+print(myname)
