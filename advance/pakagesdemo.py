66f8d1ad40eb21b85c1f595912a23947420161d4
Gaurav Rajput <105104456+Gauravraj1141@users.noreply.github.com>
2023-04-12 15:25:09 +0530

    new file created

--- advance/pakagesdemo.py
+++ advance/pakagesdemo.py
@@ -6,27 +6,5 @@ import pandas
 # print(data.items)
 
 
-# some string functions let's deep dive into it
 
-# name = "Gaurav Rajput"
-# name = "ramv Rajput"
-#
-# print(name.casefold())  # it will convert upper case into lower case
-# print(name.isspace())
-#
-# here we can write a logic here for write a name like title first letter is capital and other will small
-# print(name.istitle())
-
-
-def title(name):
-    if not name.istitle():
-        name = name.title()
-    return name
-
-
-data = 'ram singh'
-
-
-myname = title(data)
-print(myname)
 
