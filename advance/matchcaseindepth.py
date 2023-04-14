5ad3a77222a167a22da20ccb9980668579fc64a9
Gaurav Rajput <105104456+Gauravraj1141@users.noreply.github.com>
2023-04-14 21:04:39 +0530

    match case expalined

---
+++ advance/matchcaseindepth.py
@@ -0,0 +1,33 @@
+table = int(input("Enter a number which number's table you want to show: "))
+
+match table:
+    case 1:
+        for i in range(10):
+            print(i+1)
+    case 2:
+        for i in range(10):
+            print((i+1)*2)
+    case 3:
+        for i in range(10):
+            print((i+1)*3)
+    case 4:
+        for i in range(10):
+            print((i+1)*4)
+    case 5:
+        for i in range(10):
+            print((i+1)*5)
+    case 6:
+        for i in range(10):
+            print((i+1)*6)
+    case 7:
+        for i in range(10):
+            print((i+1)*7)
+    case 8:
+        for i in range(10):
+            print((i+1)*8)
+    case 9:
+        for i in range(10):
+            print((i+1)*9)
+    case 10:
+        for i in range(10):
+            print((i+1)*10)
